348d8ea400ee8630c29278af5f815885bcc19e74
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2024-10-06 12:14:05 +0530

    add sidebar active links fix how it works button background color and text color on hover and static

--- a.py
+++
@@ -1,3 +0,0 @@
-import re
-
-print(re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", "abac@gmail.com"))
--- script.js
+++ script.js
@@ -14,16 +14,14 @@ hamburger.addEventListener("click", () => {
 	// navLinks.classList.toggle("active");
 	const element = document.querySelector(".sidebar");
 	const curr = element.getAttribute("open");
-	if (!curr){
+	if (!curr) {
 		element.style.transform = "translateX(0px)";
 		element.setAttribute("open", "open");
-	}
-	else{
-		if (curr === "open"){
+	} else {
+		if (curr === "open") {
 			element.style.transform = "translateX(-100%)";
 			element.setAttribute("open", "closed");
-		}
-		else{
+		} else {
 			element.style.transform = "translateX(0px)";
 			element.setAttribute("open", "open");
 		}
@@ -72,6 +70,41 @@ function showPrevSlide() {
 	carousel.style.transform = `translateX(-${index * 100}%)`;
 }
 
-nextBtn.addEventListener("click", showNextSlide);
-prevBtn.addEventListener("click", showPrevSlide);
+nextBtn?.addEventListener("click", showNextSlide);
+prevBtn?.addEventListener("click", showPrevSlide);
 
+// script for handling active links and showing wave svg
+const links = new Map();
+
+links.set("/", "home");
+links.set("home", "home");
+
+links.set("corporates", "corporates");
+links.set("contact", "contact");
+links.set("pricing", "pricing");
+const currentUrl = window.location.href;
+links.keys().forEach((key) => {
+	if (currentUrl.endsWith(key)) {
+		document.getElementById(links.get(key)).classList.add("active");
+		document
+			.getElementById(links.get(key))
+			.children[1].classList.remove("hidden");
+	} else {
+		document.getElementById(links.get(key)).classList.remove("active");
+		document.getElementById(links.get(key)).children[1].classList.add("hidden");
+	}
+});
+
+const navlinks = document.getElementsByClassName("nav-link");
+
+for (let i = 0; i < navlinks.length; i++) {
+	navlinks[i].addEventListener("click", function () {
+		const current = document.getElementsByClassName("active");
+		const children = current[0].children;
+		children[1].classList.add("hidden");
+
+		current[0].classList.remove("active");
+		this.classList.add("active");
+		this.children[1].classList.remove("hidden");
+	});
+}
